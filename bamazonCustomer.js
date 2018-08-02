eb0e4793042acb67f41a32fa76d3b918b8f18bc6
chadbalash <chad@chadbalash.com>
2018-08-02 15:54:38 -0700

    created order function and inventory check

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,18 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
   
     // Your port; if not 3306
     port: 3306,
   
     // Your username
-    user: "balasher",
+    user: 'balasher',
   
     // Your password
-    password: "3201111",
-    database: "bamazon"
+    password: '3201111',
+    database: 'bamazon'
   });
   
     // connection to mysql
@@ -27,23 +27,24 @@ const connection = mysql.createConnection({
   function runShop() {
     inquirer
       .prompt({
-        name: "action",
-        type: "rawlist",
-        message: "\nWhat would you like to do?",
+        name: 'action',
+        type: 'rawlist',
+        message: '\nWhat would you like to do?',
         choices: [
           'Shop store',
           'Exit store'
         ]
       })
       .then(function(answer) {
+
         switch (answer.action) {
 
-        case "Shop store":
-            console.log('\nGreat! \nPlease follow the prompts to place your order.');
-            connection.end();
+        case 'Shop store':
+            console.log('\nAwesome! Please follow the prompts to place your order.\n');
+            order();
           break;
   
-        case "Exit store":
+        case 'Exit store':
             console.log('\nExiting store. Have a Safe Day!');
             connection.end();
           break;
@@ -51,7 +52,6 @@ const connection = mysql.createConnection({
         default:
         console.log('\nPlease select an option.');
           break;
-  
         }
       });
   }
@@ -63,9 +63,39 @@ const connection = mysql.createConnection({
       connection.query('SELECT item_id, product_name, price FROM products', function(err, res) {
           if (err) throw err;
           for (let i = 0; i < res.length; i++) {
-            console.log("Item#: " + res[i].item_id + " || Description: " + res[i].product_name + " || Price: " + res[i].price);
+            console.log('Item#: ' + res[i].item_id + ' || Description: ' + res[i].product_name + ' || Price: ' + res[i].price);
           }
           runShop();
-        //   connection.end();
+      })
+  }
+
+
+  function order() {
+    inquirer
+      .prompt([
+        {
+          name: 'itemID',
+          type: 'input',
+          message: 'Enter the item number of the product you would like to purchase.',
+        },
+        {
+          name: 'quantity',
+          type: 'input',
+          message: 'How many units would you like to purchase?',
+        }
+      ])
+      .then(function(answer){
+        connection.query('SELECT * FROM products WHERE ?', {item_id: answer.itemID}, function(err, res) {
+          console.log('Item#: ' + res[0].item_id + ' || Description: ' + res[0].product_name + ' || Price: ' + res[0].price);
+          let stockAvailable = res[0].stock_quantity;
+          let quantityRequest = answer.quantity;
+          if(quantityRequest > stockAvailable) {
+            console.log('There is not enough stockAvailable...');
+          } else {
+            console.log('We have enough stock');
+          }
+
+        });
+        // console.log(answer.itemID + ' ' + answer.quantity);
       })
   }
